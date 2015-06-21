e3d1a281d2f7ef88cc4e09cf83f90f1b9867c2bb
Phil Costin <philcostin@hotmail.com>
2015-06-22 00:14:04 +0100

    Minor fire fixes

--- cityscript/gamemode/schemas/cityscript/plugins/city_fire.lua
+++ cityscript/gamemode/schemas/cityscript/plugins/city_fire.lua
@@ -4,7 +4,7 @@
 
 PLUGIN.Name = "Spreadable Fire"; -- What is the plugin name
 PLUGIN.Author = "philxyz"; -- Author of the plugin
-PLUGIN.Description = "Spreadable fire code."; -- The description or purpose of the plugin
+PLUGIN.Description = "Spreadable fire code from SeriousRP gamemode"; -- The description or purpose of the plugin
 
 FlammableSENTs =
 {
@@ -32,59 +32,96 @@ function IsFlammable(ent)
 	return false
 end
 
-function FireSpread(e)
-	if e:IsOnFire() then
-		if e:GetTable().MoneyBag then
-			for k, v in pairs(player.GetAll()) do
-				CAKE.Response(v, TEXT.FireShock(e:GetTable().Amount))
-			end
-			e:Remove()
-		end
+function LightOthers(igniter)
+	-- Find all the props within a radius of 20 - 90 units of the burning prop
+	math.randomseed(os.time())
+	local nearbyEnts = ents.FindInSphere(igniter:GetPos(), math.random(20, 90))
 
-		local en = ents.FindInSphere(e:GetPos(), math.random(20, 90))
+	-- Sort them by closest first
+	table.sort(nearbyEnts, function(a, b) return igniter:GetPos():Distance(a:GetPos()) < igniter:GetPos():Distance(b:GetPos()) end)
 
-		local maxcount = 3
-		local count = 1
-		local rand = 0
+	-- If there is nothing in range, do nothing.
+	if #nearbyEnts == 0 then return end
 
-		for k, v in pairs(en) do
-			if IsFlammable(v) and v:GetClass() ~= "atm" then
-				if count >= maxcount then break end
-				if math.random(0.0, 6000.0) < 1.0 then
-					if not v.burned then
-						v:Ignite(math.random(5,180), 0)
-						v.burned = 1
-					else
-						local col = v:GetColor()
-						if (col.r - 51)>=0 then col.r = col.r - 51 end
-						if (col.g - 51)>=0 then col.g = col.g - 51 end
-						if (col.b - 51)>=0 then col.b = col.b - 51 end
-						v:SetColor(col)
-						math.randomseed((col.r / (col.g+1)) + col.b)
-						if (col.r+col.g+col.b) < 103 and math.random(1, 100) < 35 then
-							v:Fire("enablemotion","",0)
-							constraint.RemoveAll(v)
-						end
+	-- Starting with the closest prop
+	for _, prop in ipairs(nearbyEnts) do
+		-- If the prop found is not the prop which is trying to
+		-- spread its flames and the random number matches
+		if prop:EntIndex() ~= igniter:EntIndex() and math.random(1, 7) == 1 then
+			-- If the prop that the fire is trying to spread to
+			-- has already been considered to be burned
+			if prop.HasBurned then
+				-- Darken it a little instead
+				local col = prop:GetColor()
+				if (col.r - 51)>=0 then col.r = col.r - 51 end
+				if (col.g - 51)>=0 then col.g = col.g - 51 end
+				if (col.b - 51)>=0 then col.b = col.b - 51 end
+				prop:SetColor(col)
+				math.randomseed(os.time())
+
+				-- If it is burned enough by this point,
+				-- break constraints so that burned structures collapse.
+				if (col.r+col.g+col.b) < 103 and math.random(1, 100) < 35 then
+					prop:Fire("enablemotion","",0)
+					constraint.RemoveAll(prop)
+				end
+			else
+				-- Set the prop on fire
+				math.randomseed(os.time())
+				if prop:IsPlayer() then
+					-- Players should only burn for 3 seconds
+					-- otherwise when they spawn, you can't
+					-- put the fire out again and they die
+					-- repeatedly
+					prop:Ignite(3, 0)
+				else
+					-- This is a prop
+					prop:Ignite(math.random(5, 180), 0)
+					prop.HasBurned = true
+				end
+
+				-- When burning money, report a sad story.
+				if prop:GetTable().MoneyBag then
+					prop:Remove()
+					for k, v in pairs(player.GetAll()) do
+						CAKE.Response(v, TEXT.FireShock(e:GetTable().Amount))
 					end
 				end
 			end
+
+			break -- after igniting one prop, let the script continue
 		end
 	end
 end
 
 function FireThink()
-	local php = ents.FindByClass("prop_physics")
-
-	for k, v in pairs(php) do
-		FireSpread(v)
+	-- If the time to burn has not arrived, get out of the way
+	if NextFireThink and
+		CurTime() < NextFireThink then
+		return
 	end
 
-	for _, v in ipairs(FlammableSENTs) do
-		local en = ents.FindByClass(v)
-
-		for a, b in ipairs(en) do
-			FireSpread(b)
+	-- Are there any burning SENTs that are listed as flammable?
+	-- These should spread their fire.
+	for _, flam in ipairs(FlammableSENTs) do
+		for _, prop in ipairs(ents.FindByClass(flam)) do
+			if prop:IsOnFire() then
+				LightOthers(prop)
+			end
 		end
+	end
+
+	-- Burning props should have the opportunity to spread their fire to
+	-- one other prop (maybe)
+	for _, prop in pairs(ents.FindByClass("prop_physics")) do
+		if prop:IsOnFire() then
+			LightOthers(prop)
+		end
+	end
+
+	-- Calculate when the body of this hook should run next
+	if NextFireThink == nil or CurTime() >= NextFireThink then
+		NextFireThink = CurTime() + math.random(1.5, 3.78)
 	end
 end
 hook.Add("Think", "FireThink", FireThink)
