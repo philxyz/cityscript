17b77ca57c658b7a041d4a5a89d999e5e8ca045f
Phil Costin <philcostin@hotmail.com>
2015-10-05 00:05:38 +0100

    Remove Toxics Lab

--- cityscript/gamemode/schemas/cityscript/items/toxicslab.lua
+++
@@ -1,8 +0,0 @@
-ITEM.Name = "Toxics Lab";
-ITEM.Class = "toxicslab";
-ITEM.Sent = "toxic_lab";
-ITEM.Description = "Careful, now!";
-ITEM.Model = "models/props_combine/combine_mine01.mdl";
-ITEM.Purchaseable = true;
-ITEM.Price = 300;
-ITEM.ItemGroup = 5;
