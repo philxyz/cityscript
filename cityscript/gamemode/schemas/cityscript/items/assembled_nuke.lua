4e2909c05cc42185992af812bb26fcd8a02f2837
Phil Costin <philcostin@hotmail.com>
2015-10-04 23:54:34 +0100

    Remove full nuke

--- cityscript/gamemode/schemas/cityscript/items/assembled_nuke.lua
+++
@@ -1,8 +0,0 @@
-ITEM.Name = "Nuclear Bomb";
-ITEM.Class = "assembled_nuke";
-ITEM.Sent = "sent_nuke_detpack"
-ITEM.Description = "End it all.";
-ITEM.Model = "models/weapons/w_c4.mdl";
-ITEM.Purchaseable = true;
-ITEM.Price = 50000;
-ITEM.ItemGroup = 6;
